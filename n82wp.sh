3951b0bc361b8fe6ed27ed2d7e97547dda301907
Nemanja Djuric <ne@nemanja.io>
2016-11-01 20:24:20 +0100

    Update n82wp.sh

--- n82wp.sh
+++ n82wp.sh
@@ -15,19 +15,60 @@ cat <<EOF
     Usage: 
     bash n82wp.sh update           Update Wordpress theme and plugins.
     bash n82wp.sh repair           This will repair current Wordpress installation with backup in case of disaster.
-    bash n82wp.sh scan             Scan site with Sucuri, Install Wordfence scanner, Create temp. username and email results. 
+    bash n82wp.sh scan             Scan site with Sucuri, Install Wordfence scanner, Create temp. username and email results.
+    bash n82wp.sh selfupdate 	   Update n82wp application
     bash n82wp.sh -h               show this help.
 EOF
 }
 
+function selfupdate(){
+SELF=$(basename $0)
+updatebase=$"https://raw.githubusercontent.com/nemke82/n82wp/master"
+  echo "Checking for updates..."
+    # Download new version
+  echo -n "Downloading latest version..."
+  if ! wget --quiet --output-document="$0.tmp" $updatebase/$SELF ; then
+    echo "Failed: Error while trying to wget new version!"
+    echo "File requested: $updatebase/$SELF"
+    exit 1
+  fi
+  echo "Done."
+  # Copy over modes from old version
+  OCTAL_MODE=$(stat -c '%a' $SELF)
+  if ! chmod $OCTAL_MODE "$0.tmp" ; then
+    echo "Failed: Error while trying to set mode on $0.tmp."
+    exit 1
+  fi
+  # Spawn update script
+  cat > updateScript.sh << EOF
+#!/bin/bash
+# Overwrite old file with new
+if mv "$0.tmp" "$0"; then
+  echo "Done. Update complete."
+  rm \$0
+else
+  echo "Failed!"
+fi
+EOF
+
+  echo -n "Inserting update process..."
+  exec /bin/bash updateScript.sh
+
+}
+
+
 function update(){
     if [[ "$param1" == "update" ]];then
-    echo "Checking if wp-cli tool is installed".
-    elif [ ! -f /usr/local/bin/wp ];then
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    wpcli=$"/usr/local/bin/wp"
+    if [ -f $wpcli ]
+        then
+echo "WP CLI Exists."
+    else
+echo "WP CLI Installed." &&
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&
     chmod +x wp-cli.phar &&
     sudo mv wp-cli.phar /usr/local/bin/wp
-    fi
+fi
     echo "NOTE: Checking Wordpress installation"
     VERSION=$(wp core version --allow-root)
     THEME=$(wp theme status --allow-root)
@@ -51,9 +92,13 @@ fi
 
 function repair(){
     if [[ "$param1" == "repair" ]];then
-    echo "Checking if wp-cli tool is installed".
-    elif [ ! -f /usr/local/bin/wp ];then
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    wpcli=$"/usr/local/bin/wp"
+    if [ -f $wpcli ]
+        then
+echo "WP CLI Exists."
+    else
+echo "WP CLI Installed." &&
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&
     chmod +x wp-cli.phar &&
     sudo mv wp-cli.phar /usr/local/bin/wp
 fi
@@ -82,68 +127,78 @@ fi
 }
 
 function scan(){
-    if [[ "$param1" == "scan" ]];then
-    echo "Checking if wp-cli tool is installed".
-    elif [ ! -f /usr/local/bin/wp ];then
-    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+wpcli=$"/usr/local/bin/wp"
+    if [ -f $wpcli ]
+	then
+echo "WP CLI Exists."
+    else
+echo "WP CLI Installed." &&	
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar &&
     chmod +x wp-cli.phar &&
     sudo mv wp-cli.phar /usr/local/bin/wp
 fi
 echo "Clearing tmp..."
 rm -f /tmp/malwarecheck.txt &&
-rm -f /tmp/malwarecheckemail.txt
+rm -f /tmp/malwarecheckemail.txt &&
+echo "Fixing ServerPilot wp-config.php..." &&
+find . -name "wp-config.php" -print | xargs sed -i 's/define'\(''\''WP_SITEURL'\'''\,' SP_REQUEST_URL'\)''\;'//g' &&
+find . -name "wp-config.php" -print | xargs sed -i 's/define'\(''\''WP_HOME'\'''\,' SP_REQUEST_URL'\)''\;'//g'
 SITE=$(wp option get siteurl --allow-root | cut -c 8-)
 EMAIL="$param2"
 HOSTNAME=$(hostname)
 PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
 SENDEMAIL=0
 
-if [[ "$EMAIL" == "" ]] 
+if [[ "$EMAIL" == "" ]]
 then
 EMAIL="root@$HOSTNAME"
 fi
 
 for s in $SITE
 do
-	WARNING=0
-	> /tmp/malwarecheck.txt
-	links -dump https://sitecheck.sucuri.net/results/$s | sed -n "/Infected/p" >> /tmp/malwarecheck.txt
-	while read line; do
-		if [[ "$line" == "[15]Infected with Malware" ]]
-		then
-			WARNING=1
-		fi
-	done < /tmp/malwarecheck.txt
-	if [ $WARNING -eq 1 ]
-	then
-        	SENDEMAIL=1
-		echo "https://sitecheck.sucuri.net/results/$s" >> /tmp/malwarecheckemail.txt
-		cat /tmp/malwarecheck.txt >> /tmp/malwarecheckemail.txt
-		echo "" >> /tmp/malwarecheckemail.txt
-		echo "" >> /tmp/malwarecheckemail.txt
+        WARNING=0
+        > /tmp/malwarecheck.txt
+        links -dump https://sitecheck.sucuri.net/results/$s | sed -n "/Infected/p" >> /tmp/malwarecheck.txt
+        while read line; do
+				if [[ "$line" == "[15]Infected with Malware" ]]
+                then
+                        WARNING=1
+                fi
+        done < /tmp/malwarecheck.txt
+        if [ $WARNING -eq 1 ]
+        then
+                SENDEMAIL=1
+                echo "https://sitecheck.sucuri.net/results/$s" >> /tmp/malwarecheckemail.txt
+                cat /tmp/malwarecheck.txt >> /tmp/malwarecheckemail.txt
+                echo "" >> /tmp/malwarecheckemail.txt
+                echo "" >> /tmp/malwarecheckemail.txt
 echo "MALWARE DETECTED!!! https://sitecheck.sucuri.net/results/$s"
-	fi
+        fi
 done
 if [ $SENDEMAIL -eq 1 ]
 then
 echo "Sending email if specified..."
+fi
 
 wordfence=$(pwd -P)"/wp-content/plugins/wordfence"
 echo "Checking if Wordfence plugin is installed"
-    elif [ ! -d $wordfence ];then
+    if [ -d $wordfence ]
+	then
+echo "Wordfence plugin already exists."
+	else
 echo "Installing Wordfence plugin."
 wp plugin install wordfence --allow-root &&
 echo "Activating plugin."
 wp plugin activate wordfence --allow-root
-fi
+	fi
 wp user create temp$PASSWORD $EMAIL --role=administrator --user_pass=$PASSWORD --allow-root &&
-echo -e "Please login to the Dashobard of $SITE website with following details: \n" >> /tmp/malwarecheckemail.txt
+echo -e "Please login to the Dashboard $SITE using following details: \n" >> /tmp/malwarecheckemail.txt
 echo -e "username: temp$PASSWORD \n" >> /tmp/malwarecheckemail.txt
 echo -e "password: $PASSWORD \n" >> /tmp/malwarecheckemail.txt >> /tmp/malwarecheckemail.txt
 
 cat /tmp/malwarecheckemail.txt &&
 
-mail -s "URGENT: Malware detected on $SITE! Wordfence installed,review site!" $EMAIL < /tmp/malwarecheckemail.txt
+mail -s "URGENT: Malware detected! Wordfence installed,review site!" $EMAIL < /tmp/malwarecheckemail.txt
 echo "This is completed, good luck."
 }
 
@@ -162,6 +217,8 @@ function main(){
                 repair;
             elif [[ "$param1" == "scan" ]];then
                 scan;
+	    elif [[ "$param1" == "selfupdate" ]];then
+                selfupdate;
             else
 
                 show_help;
